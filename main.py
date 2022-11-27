5b5b468497cc09556e9d4fc1fe05079eb916b3b5
jbazel <joshjamesburns2002@gmail.com>
2022-11-27 14:07:53 +0000

    updated skeletons with comments

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # main.py
 
+import csv
 import pandas as pd
-import numpy
-import matplotlib
+import numpy as np
+import matplotlib as plt
 from flask import Flask
 from flask import render_template
 from flaskwebgui import FlaskUI
@@ -13,17 +14,11 @@ app = Flask(__name__)
 def processData(rawData):
     """
     perform all analysis on raw data
-
+    create dataFrames
+    analyse
     """
 
-
-def dataReport(data):
-    """
-    take in processed data
-    use pandas to generate any data-frames and perform analysis
-    return html data forms
-
-    """
+    data = pd.read_csv(rawData, index_col=0, parse_dates=True)
 
 
 def generateFigures(data):
@@ -31,6 +26,27 @@ def generateFigures(data):
     take in processed data
     generate figures and graphs in matplotlib
     save figures as files to use in HTML report
+
+    NOTES ON USiNG PANDAS AND MATPLOTLIB TO DRAW PLOTS:
+
+    NOTE: this works on whole data, series, and dataFrame
+
+    plotting all data:
+        >data.plot()
+        >plt.show()
+
+    plotting only one column:
+        >data["<VARIABLE NAME>"].plot()
+        >plt.show()
+
+    plotting different graphs:
+        >data.plot.<GRAPH/PLOT TYPE>(parameters)
+        >plt.shot()
+
+        example:
+        >data.plot.scatter(x = <VARIABLE 1>, y = <VARIABLE 2>, ...)
+        >plt.show()
+
 
     """
 
