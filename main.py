0d44cf71fde81b94bcfa3a948a648dcdf2e15457
jbazel <joshjamesburns2002@gmail.com>
2022-11-27 13:53:50 +0000

    feat: skeletons for functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # main.py
 
 import pandas as pd
+import numpy
+import matplotlib
 from flask import Flask
 from flask import render_template
 from flaskwebgui import FlaskUI
@@ -8,16 +10,41 @@ from flaskwebgui import FlaskUI
 app = Flask(__name__)
 
 
+def processData(rawData):
+    """
+    perform all analysis on raw data
+
+    """
+
+
+def dataReport(data):
+    """
+    take in processed data
+    use pandas to generate any data-frames and perform analysis
+    return html data forms
+
+    """
+
+
+def generateFigures(data):
+    """
+    take in processed data
+    generate figures and graphs in matplotlib
+    save figures as files to use in HTML report
+
+    """
+
+
+def generateReport():
+    """
+    TO DO
+    """
+
+
 @app.route("/")
 def hello():
     return render_template('index.html')
 
-
-# This is method for accessing uploaded reports
-
-# @app.route("/home", methods=['GET'])
-# def home():
-#    return render_template('Sewanalyst.html')
 
 if __name__ == "__main__":
     FlaskUI(app=app, server="flask").run()
