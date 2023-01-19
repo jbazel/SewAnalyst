2329d59a34e223133be8305e6c5586a36734ac34
joshburns <92578127+jbazel@users.noreply.github.com>
2023-01-19 16:57:38 +0000

    what

--- python-backend/processData.py
+++ python-backend/processData.py
@@ -8,9 +8,11 @@ def processData(rawData):
     analyse
     """
 
-    headers: list = ["Site Code","Site Name","IDWF l/s","Maximum Infiltration Rate (Imax, l/s)","Note","Site Name","Previous max infiltration rate where known","Maximum Infiltration Rate (Imax, l/s)","Annual Return Population Equivalent (2020) (Unrounded)","Previous Trade Effluent where known,Trade Effluent (l/day) (2020)","Water Resource Zone","TW Est. of per capita domestic flow in this area (l/hd/day)"]
+    headers: list = ["Site Code","Site Name","IDWF l/s","Maximum Infiltration Rate (Imax, l/s)","Note","Site Name 2","Previous max infiltration rate where known","Maximum Infiltration Rate (Imax, l/s) 2","Annual Return Population Equivalent (2020) (Unrounded)","Previous Trade Effluent where known,Trade Effluent (l/day) (2020)","Water Resource Zone","TW Est. of per capita domestic flow in this area (l/hd/day)"]
 
     rawData: pd.DataFrame = pd.read_csv('python-backend/sampleData.csv', names=headers) #names=headers
+    rawData = rawData.drop(rawData['Site Name 2'])
+    rawData = rawData.drop(rawData['Maximum Infiltration Rate (Imax, l/s) 2'])
     rawData.replace(' ', np.NaN)
     rawData.replace('  no data ', np.NaN)
     rawData.replace('', np.NaN)
