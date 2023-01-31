3be4710f799b845fa3b34d0fc63bf409d263a8cc
Jasmine <jasmine.chloe21@gmail.com>
2023-01-31 10:32:30 +0000

    committing all 2438 changes to website - whoops

---
+++ website-frontend/client/index.js
@@ -0,0 +1,14 @@
+function downloadFunction(){
+    alert("Are you really sure?");
+}
+
+const downloadReport = document.getElementById("getReport")
+downloadReport.addEventListener("click", async(event) => {
+    try{
+        console.log("Download button clicked")
+        const response = await fetch('http://127.0.0.1:8090/single')
+    }
+    catch(err){
+        console.log(err)
+    }
+});
