c13b2f6210284374874dff70a5726e4f94cb0841
joshburns <92578127+jbazel@users.noreply.github.com>
2023-01-31 14:57:46 +0000

    big changes to API

--- website-frontend/server/script.js
+++ website-frontend/server/script.js
@@ -7,9 +7,9 @@ app.use(express.static('client'));
 app.unsubscribe(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const reportFolderPath = 'files/report1.pdf'
-const programFolderPath = 'files/program1.json'
-app.get('/report:reportID',function(req,res) {
+const reportFolderPath = 'files/reports/report1.pdf'
+const programFolderPath = 'files/program/program1.json'
+app.get('/report/report1.pdf',function(req,res) {
     console.log('single file');
      
     // Download function provided by express
@@ -37,6 +37,17 @@ app.get('/program_download', function(req, res){
     })
 });
 
+app.post('/report_upload', function(req, res){
+    console.log(req.body);
+    fs.writeFile(reportFolderPath, req.body, function(err){
+        if(err) {
+            console.log(err);
+        }
+        else{
+            console.log('File uploaded');
+        }
+    })
+});
 
 app.listen(8090)
 console.log('Server started at http://127.0.0.1:8090');
