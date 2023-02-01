8c796d94ac6ad43ba77847b37b220e593909b285
Jasmine <jasmine.chloe21@gmail.com>
2023-02-01 11:05:14 +0000

    post request attempt

--- website-frontend/client/index.js
+++ website-frontend/client/index.js
@@ -24,4 +24,39 @@ downloadSoftware.addEventListener("click", async function(event){
     }
   });
 
+const flagReport = document.getElementById('flagReportForm')
+flagReport.addEventListener('submit', async function(event){
+    try{
+        console.log("Here")
+        reportNum = flagReport.elements.namedItem('InputReportNumber').value
+        console.log(reportNum)
+        reportDate = flagReport.elements.namedItem('InputDateOfSubmission').value
+        console.log(reportDate)
+        reportReason = flagReport.elements.namedItem('InputReason').value
+        console.log(reportReason)
+        const reportData = {reportNum: reportNum, reportDate: reportDate, reportReason: reportReason}
+        const response = await fetch('http://127.0.0.1:8090/flagReport',{
+            method: 'POST',
+            headers:{'Content-Type':'application/json'},
+            body: JSON.stringify(reportData),
+        })
+        if (response.status === 204){
+            console.log(response)
+            window.alert("Error, could not flag report")
+        }
+        else{
+            const data = await response.text()
+
+            flagReport.elements.namedItem('InputReportNumber').value = ''
+            reportDate = flagReport.elements.namedItem('InputDateOfSubmission').value = ''
+            reportReason = flagReport.elements.namedItem('InputReason').value = ''
+
+            window.alert(data);
+        }
+    }
+    catch(e){
+        alert(e)
+    }
+});
+
 
--- website-frontend/server/script.js
+++ website-frontend/server/script.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const { response } = require('express');
 
 app.use(express.static('client'));
 app.unsubscribe(bodyParser.urlencoded({ extended: false }));
@@ -9,6 +10,8 @@ app.use(bodyParser.json());
 
 const reportFolderPath = 'files/reports/report1.pdf'
 const programFolderPath = 'files/program/program1.json'
+const flagReportFolderPath = 'files/data/flaggedReports.json'
+
 app.get('/report/report1.pdf',function(req,res) {
     console.log('single file');
      
@@ -43,6 +46,45 @@ app.post('/report_upload', function(req, res){
             console.log('File uploaded');
         }
     })
+});
+
+app.post('/flagReport', (req, res)=>{
+    try{
+        info = req.body
+        print(info)
+        let reportNum = info.reportNum
+        let reportDate = info.reportDate
+        let reportReason = info.reportReason
+
+        print(reportNum)
+        print(reportDate)
+        print(reportReason)
+
+        if (!fs.existsSync(flagReportFolderPath)){
+            fs.closeSync(fs.openSync(flagReportFolderPath, 'w'));
+        }
+
+        const file = fs.readFileSync(flagReportFolderPath)
+        const data = {
+            "reportNum": reportNum,
+            "reportDate": reportDate,
+            "reportReason": reportReason
+        }
+
+        if (file.length == 0){
+            fs.writeFileSync(flagReportFolderPath, JSON.stringify([data]))
+        }
+        else{
+            const json = JSON.parse(file.toString())
+            json.push(data)
+            fs.writeFileSync(flagReportFolderPath, JSON.stringify(json, null, 2))
+            response.send("success")
+        }
+
+    }
+    catch(e){
+        response.send(e)
+    }
 });
 
 app.listen(8090)
