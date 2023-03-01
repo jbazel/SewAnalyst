fdc6251dcb60cc23a2a4367001e6a698c08b576a
joshburns <92578127+jbazel@users.noreply.github.com>
2023-03-01 16:20:32 +0000

    slight reformat

--- python-backend/figureGeneration.py
+++ python-backend/figureGeneration.py
@@ -13,34 +13,6 @@ def generateFigures(dates, PEActual, PEForecast, DWFActual, DWFForecast):
     genFigPE(dates, PEActual, PEForecast)
     genFigDWF(dates, DWFActual, DWFForecast)
 
-    """
-    take in processed data
-    generate figures and graphs in matplotlib
-    save figures as files to use in HTML report
-
-    NOTES ON USiNG PANDAS AND MATPLOTLIB TO DRAW PLOTS:
-
-    NOTE: this works on whole data, series, and dataFrame
-
-    plotting all data:
-        >data.plot()
-        >plt.show()
-
-    plotting only one column:
-        >data["<VARIABLE NAME>"].plot()
-        >plt.show()
-
-    plotting different graphs:
-        >data.plot.<GRAPH/PLOT TYPE>(parameters)
-        >plt.shot()
-
-        example:
-        >data.plot.scatter(x = <VARIABLE 1>, y = <VARIABLE 2>, ...)
-        >plt.show()
-
-
-    """
-
 
 def genFigPE(dates, PEActual, PEForecast):
     ## plot onto one graph
